68b65b44d27618e47a1535c99ac9f338f284075c
The Richie <118231354+richie20502@users.noreply.github.com>
2025-02-06 01:01:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 from flask import Flask
 from config import db, migrate
 from dotenv import load_dotenv
-import os
 from flask_cors import CORS
+from flasgger import Swagger
+import os
 
 # Cargar variables de entorno
 load_dotenv()
 
 # Crear instancia de Flask
-app = Flask(__name__) 
+app = Flask(__name__)
 CORS(app)
 
 # Configuración de la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+app.config['SWAGGER'] = {
+    'title': 'Mi API Flask',
+    'uiversion': 3
+}
+
 # Inicializar extensiones
 db.init_app(app)
 migrate.init_app(app, db)
+
+swagger = Swagger(app)
 
 # Registrar rutas
 from routes.user import user_bp
